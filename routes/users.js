ea943e56c29e0918c4963bc9043bf8c7e7a01ef0
Rafał <wawraf@gmail.com>
2018-10-19 22:20:03 +0200

    Update routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('This is just a test :)');
+  res.send('This is just a test');
 });
 
 module.exports = router;
